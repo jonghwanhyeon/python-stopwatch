2a15a381ce644b64110caaa0ef4f1280d808ebb9
Jonghwan Hyeon <hyeon0145@gmail.com>
2021-05-18 14:33:53 +0900

    Version 1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as input_file:
 
 setup(
     name='python-stopwatch',
-    version='1.0.2',
+    version='1.0.3',
     author='Jonghwan Hyeon',
     author_email='hyeon0145@gmail.com',
     description='A simple stopwatch for measuring code performance',
