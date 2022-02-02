6917496ee9d00d823414c2bf3bdf758fe3e99d60
Rafael <rafael271alves@gmail.com>
2022-02-02 10:20:26 -0300

    add Caller typing

--- stopwatch/contextmanagers/__init__.py
+++ stopwatch/contextmanagers/__init__.py
@@ -1,13 +1,20 @@
 import inspect
-from collections import namedtuple
+from typing import NamedTuple
 
-Caller = namedtuple('Caller', ['module', 'function', 'line_number'])
+
+class Caller(NamedTuple):
+    module: str
+    function: str
+    line_number: int
 
 
 def inspect_caller(offset: int = 0) -> Caller:
     stack = inspect.stack()[2 + offset]
-    return Caller(module=inspect.getmodule(stack.frame).__name__,
-                    function=stack.function, line_number=stack.lineno)
+    module = inspect.getmodule(stack.frame)
+    return Caller(module=module.__name__ if module else '<unknown>',
+                  function=stack.function,
+                  line_number=stack.lineno)
+
 
 def format_elapsed_time(elapsed: float) -> str:
     return f'{elapsed:.4f}s' if elapsed >= 0.1 else f'{elapsed * 1000:.2f}ms'
